f78c2850908e8b6aafe2c40d4be860b168f8f258
alexj88 <a.johny87@gmail.com>
2025-09-01 20:28:53 -0300

    Update client.py

--- client.py
+++ client.py
@@ -8,3 +8,13 @@ try:
     print (f"status code: {response_get.status_code}")
     print("conteudo da resposta:")
     print(response_get)
+
+# requisição POST
+    data = {'nome': 'Fulano', 'idade':30}
+    response_post = requests.post(URL, data=data)
+    print("\n---Requisição POST---")
+    print(f"Status Code: {response_post.status_code}")
+    print("conteudo da resposta:")
+    print(response_post.text)
+except requests.exceptions.RequestsException as e:
+    print(f"erro ao conectar com o servidor:{e}")
